38a87f1f14da2722b96fa604bfe0184a46207b15
HelloHong <hongcn@163.com>
2021-12-16 14:13:52 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,9 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+#4. 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Hong OpenWrt'' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#5. 版本号显示名字
+sed -i "s/Openwrt /HongnahZ Build $(TZ=UTC-8 date "+%Y.%m.%d") @ Openwrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
